a257be115cf031a374fdb54f4b3266da1ebeec52
Guilherme Henrique <guilherme.scosta2001@gmail.com>
2024-11-11 14:44:39 -0300

    API para pesquisar por CEP

---
+++ resources/views/cep/resultado.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>CEP</title>
+</head>
+<body>
+
+{{ $response['state']  }} <br>
+{{ $response['city']  }}<br>
+{{ $response['neighborhood']  }}<br>
+{{ $response['street']  }}
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/user', function () {
+    return view('user._layouts.user');
+});
+
+Route::get('/api', [\App\Http\Controllers\CepController::class, 'get'])->name('api.get');
+Route::post('/api/resultado', [\App\Http\Controllers\CepController::class, 'post'])->name('api.post');

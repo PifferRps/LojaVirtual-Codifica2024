b05c344c1462b0584d60b617e1d52337e42cc0d5
Guilherme Henrique <guilherme.scosta2001@gmail.com>
2024-11-11 14:52:36 -0300

    Layouts padrão usuario com componentes

---
+++ resources/views/user/_components/navbar.blade.php
@@ -0,0 +1,32 @@
+<nav class="nav">
+    <select>
+        <option value="1">Opção</option>
+        <option value="1">Opção</option>
+        <option value="1">Opção</option>
+        <option value="1">Opção</option>
+    </select>
+    <select>
+        <option value="1">Opção</option>
+        <option value="1">Opção</option>
+        <option value="1">Opção</option>
+        <option value="1">Opção</option>
+    </select>
+    <select>
+        <option value="1">Opção</option>
+        <option value="1">Opção</option>
+        <option value="1">Opção</option>
+        <option value="1">Opção</option>
+    </select>
+    <select>
+        <option value="1">Opção</option>
+        <option value="1">Opção</option>
+        <option value="1">Opção</option>
+        <option value="1">Opção</option>
+    </select>
+    <select>
+        <option value="1">Opção</option>
+        <option value="1">Opção</option>
+        <option value="1">Opção</option>
+        <option value="1">Opção</option>
+    </select>
+</nav>

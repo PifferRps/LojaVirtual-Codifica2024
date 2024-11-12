079c72474303315070ba84a038894d52e2fbf870
João Pedro V. Pansiere <ojp027@gmail.com>
2024-11-12 13:18:24 -0300

    Layout admin adicionado

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,10 @@ Route::get('/user', function () {
     return view('user._layouts.user');
 });
 
+Route::get('/admin', function () {
+    return view('admin.pages.index');
+});
+
 Route::get('/api', [\App\Http\Controllers\CepController::class, 'get'])->name('api.get');
 Route::post('/api/resultado', [\App\Http\Controllers\CepController::class, 'post'])->name('api.post');
 

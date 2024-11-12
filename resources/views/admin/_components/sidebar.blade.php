95d79121aa8a657b3ee4da1809da7e5f980586c7
João Pedro V. Pansiere <121506083+Pansiere@users.noreply.github.com>
2024-11-12 13:34:02 -0300

    Layout admin adicionado (#10)

---
+++ resources/views/admin/_components/sidebar.blade.php
@@ -0,0 +1,19 @@
+<div class="sidebar">
+    <div class="sidebar_logo">
+        <img src="teste.png">
+    </div>
+
+    <div class="sidebar_opcoes">
+        <a href="#Dashboard">Dashboard</a>
+        <a href="#Pedidos">Pedidos</a>
+        <a href="#Produtos">Produtos</a>
+        <a href="#Fornecedor">Fornecedor</a>
+        <a href="#Categorias">Categorias</a>
+        <a href="#Relátorios">Relátorios</a>
+    </div>
+
+    <div class="sidebar_baixo">
+        <a href="#Minha conta">Minha conta</a>
+        <a href="#Ver loja">Ver loja</a>
+    </div>
+</div>

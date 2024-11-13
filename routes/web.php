3ed79691c8af2a5cfd2eeb26a87132933acbe1eb
Maryana Rigo <maryana.rigo.sch@gmail.com>
2024-11-12 23:41:17 -0300

    Tela de cadastro

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,13 @@ Route::get('/user', function () {
     return view('user._layouts.user');
 });
 
+Route::get('/cadastro', function() {
+    return view('user.cadastro');})->name('user.cadastro');
+
+Route::get('/admin', function () {
+    return view('admin.pages.index');
+});
+
 Route::get('/api', [\App\Http\Controllers\CepController::class, 'get'])->name('api.get');
 Route::post('/api/resultado', [\App\Http\Controllers\CepController::class, 'post'])->name('api.post');
 

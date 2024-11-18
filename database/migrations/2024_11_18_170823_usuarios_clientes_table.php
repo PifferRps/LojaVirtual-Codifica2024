e90f51fdc743013482ea2493a81696be7aa59656
João Pedro V. Pansiere <ojp027@gmail.com>
2024-11-18 16:14:45 -0300

    Migrations 'usuarios_clientes'

---
+++ database/migrations/2024_11_18_170823_usuarios_clientes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\Usuario;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('usuarios_clientes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Usuario::class, 'usuario_id')->constrained();
+            $table->string('nome');
+            $table->string('documento');
+            $table->timestamp('data_nascimento');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};

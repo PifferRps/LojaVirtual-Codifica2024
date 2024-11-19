f403cff9cd63f1cec46b391e5228e91ddf945ed7
gabrielpsanti <95512478+gabrielpsanti@users.noreply.github.com>
2024-11-19 17:15:11 -0300

    Corrigido problema de cache, arquivo correto enviado (#28)
    * Agora vai
    
    * Update 2024_11_19_174309_pedidos_produtos.php

---
+++ database/migrations/2024_11_19_174309_pedidos_produtos.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pedidos_produtos', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('pedido_id')->constrained('pedidos');
+            $table->foreignId('produto_id')->constrained('produtos');
+            $table->integer('quantidade');
+            $table->float('produto_valor', 10, 2);
+            $table->float('produto_valor_promocional', 10, 2);
+            $table->float('valor_total_final', 10, 2);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pedidos_produtos');
+    }
+};

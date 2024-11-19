490f0ec5173e1768b377dfadca6a0346de158306
Gabriel Santi <gabrielspessoti@gmail.com>
2024-11-19 14:43:51 -0300

    Agora vai

---
+++ database/migrations/2024_11_19_174309_pedidos_produtos.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pedidos_produtos', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('pedido_id')->constrained('pedidos');
+            $table->foreignId('produto_id')->constrained('produtos');
+            $table->integer('quantidade');
+            $table->float('produto_valor');
+            $table->float('produto_valor_promocional');
+            $table->float('valor_total_final');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pedidos_produtos');
+    }
+};

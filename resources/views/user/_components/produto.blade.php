c30d9c5e92f930b970d838f34c7579aa93a22c48
Vinícius Oliveira Mesquita <vinygiacomin@gmail.com>
2024-11-26 20:28:48 -0300

    Update produto.blade.php

--- resources/views/user/_components/produto.blade.php
+++ resources/views/user/_components/produto.blade.php
@@ -1,48 +1,67 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página Produto</title>
+    <link rel="stylesheet" href="projetao.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+</head>
+
+<body>
+
 <div class="container_blocos">
     
-<div class="blocoPrincipal">
-
-<div class="conteudoProduto">
-
-    <div class="imagemProduto">
-        <div class="imagemProdutoReferencia"></div>
-        <div class="imagemProdutoReferencia"></div>
-        <div class="imagemProdutoReferencia"></div>
-        <div class="imagemProdutoReferencia"></div>
+    <div class="blocoPrincipal">
+    
+    <div class="conteudoProduto">
+    
+        <div class="imagemProduto">
+            <div class="imagemProdutoReferencia"></div>
+            <div class="imagemProdutoReferencia"></div>
+            <div class="imagemProdutoReferencia"></div>
+            <div class="imagemProdutoReferencia"></div>
+        </div>
+        
+        <div class="informacoesProduto">
+    
+            <div class="blocoSuperior">
+                <div class="rotas">Home > Categoria ></div>
+                <div class="tituloProduto"> Titulo de produto</div>
+                <div class="descricaoSuperior">SKU:I90FGI9958</div>
+            </div>
+    
+            <div class="blocoInferior">
+                <div class="tituloProduto"> R$900,00</div>
+                <div class="descricaoSuperior">A vista no pix 10% OFF</div>
+                <div class="descricaoInferior">R$1000,00</div>
+                <div class="descricaoSuperior">Em até 10x de R$100,00 sem juros</div>
+    
+            </div>
+    
+            <div class="botoesInferiores">
+                <div class="botaoCompra"> Comprar Agora</div>
+                <div class="botaoCarrinho"> <i class="fa-solid fa-cart-shopping"></i> </div>
+            </div>
+    
+        </div>
+    
     </div>
     
-    <div class="informacoesProduto">
-
-        <div class="blocoSuperior">
-            <div class="rotas">Home > Categoria ></div>
-            <div class="tituloProduto"> Titulo de produto</div>
-            <div class="descricaoSuperior">SKU:I90FGI9958</div>
+    <div class="detalhesProduto">
+    
+        <div class="titulo">Detalhes do Produto</div>
+        <div class="descricao"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum </div>
         </div>
-
-        <div class="blocoInferior">
-            <div class="tituloProduto"> R$900,00</div>
-            <div class="descricaoSuperior">A vista no pix 10% OFF</div>
-            <div class="descricaoInferior">R$1000,00</div>
-            <div class="descricaoSuperior">Em até 10x de R$100,00 sem juros</div>
-
-        </div>
-
-        <div class="botoesInferiores">
-            <div class="botaoCompra"> Comprar Agora</div>
-            <div class="botaoCarrinho"> 🛒 </div>
-        </div>
-
+    
+     
+    
+    </div>
+    
+    </div>
     </div>
 
-</div>
+</body>
 
-<div class="detalhesProduto">
 
-    <div class="titulo">Detalhes do Produto</div>
-    <div class="descricao"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum </div>
-    </div>
-
-</div>
-
-</div>
-</div>

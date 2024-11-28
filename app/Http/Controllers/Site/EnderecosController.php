b61012a518f676ec59c1e6b056e1e6805498be11
elias <eliasbof@gmail.com>
2024-11-28 20:51:32 -0300

    falta um pouco de teste ainda

--- app/Http/Controllers/Site/EnderecosController.php
+++ app/Http/Controllers/Site/EnderecosController.php
@@ -4,36 +4,50 @@ namespace App\Http\Controllers\Site;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\ClienteEndereco;
+use App\Models\UsuarioCliente;
 
 class EnderecosController extends Controller
 {
     public function index()
     {
-        //
+        $endereco = ClienteEndereco::all(); //por enquanto ele puxa todos os pedidos
+        //$clienteId = Auth::guard('cliente')->user()->cliente_id;
+        //$cliente = UsuarioCliente::findOrFail($clienteId);
+        //$endereco = ClienteEndereco::where('cliente_id', $clienteId)->get();
+
+        //precisa do login funcionando
+        return view('user.pages.cart.list', compact('cart'));
     }
 
     public function create()
     {
-        //
+        return view('user.pages.cart.form', compact('form')); // não existe ainda
     }
 
     public function store(Request $request)
     {
-        //
+        ClienteEndereco::create($request->all());
+
+        return view('user.pages.cart.list', compact('cart')); 
     }
 
-    public function edit(string $id)
+    public function edit(ClienteEndereco $endereco)
     {
-        //
+        return view('user.pages.cart.form', compact('form')); // não existe ainda
     }
 
-    public function update(Request $request, string $id)
+    public function update(Request $request, ClienteEndereco $endereco)
     {
-        //
+        $endereco->update($request->all());
+
+        return view('user.pages.cart.list', compact('cart')); 
     }
 
-    public function destroy(string $id)
+    public function destroy(ClienteEndereco $endereco)
     {
-        //
+        $endereco->delete();
+
+        return view('user.pages.cart.list', compact('cart')); 
     }
 }

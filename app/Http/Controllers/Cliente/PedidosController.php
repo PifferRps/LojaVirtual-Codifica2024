79836200bb7cb0187907f6ce05f898c430cbc636
EB <eliasbofneto@gmail.com>
2024-11-28 11:53:41 -0300

    deve funcionar precisa do id do cliente funcionando

--- app/Http/Controllers/Cliente/PedidosController.php
+++ app/Http/Controllers/Cliente/PedidosController.php
@@ -5,22 +5,28 @@ namespace App\Http\Controllers\Cliente;
 use App\Http\Controllers\Controller;
 use App\Models\Pedido;
 use App\Models\PedidoProduto;
+use App\Models\UsuarioCliente;
 use Illuminate\Http\Request;
 
 class PedidosController extends Controller
 {
     public function index()
     {
-        $pedidos = Pedido::where('id', '=', 'cliente_id');
-        $pedidos = Pedido::where('cliente_id',$id)->Pedido();
+
+        $pedido = Pedido::all(); //por enquanto ele puxa todos os pedidos
+        //$clienteId = Auth::guard('cliente')->user()->cliente_id;
+        //$cliente = UsuarioCliente::findOrFail($clienteId);
+        //$pedido = Pedido::where('cliente_id', $clienteId)->where('status', Status::Ativo)->get();
+
+        //precisa do login funcionando
         return view('user.pages.purchases.list', compact('purchases'));
     }
 
-    public function show(Pedido $pedido)
+    public function show(Request $request, int $pedidoId)
     {
-        $Pedidoproduto = PedidoProduto::all();
+        $Pedidoproduto = PedidoProduto::where('pedido_id', $pedidoId)->get();
 
-        return view('user.pages.purchase', compact('purchase'));
+        return view('user.pages.purchase', compact('purchase')); //pagina de pedido especifico não existe ainda
     }
 
 }

a35e8ca3263cd4381b9c3d12bac7e2b432f31967
Gabriel Santi <gabrielspessoti@gmail.com>
2024-12-03 09:43:05 -0300

    Protótipo do dompdf funcionando e algumas Models corrigidas

--- app/Http/Controllers/Admin/RelatoriosController.php
+++ app/Http/Controllers/Admin/RelatoriosController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use Dompdf\Dompdf;
 use Illuminate\Http\Request;
 
 class RelatoriosController extends Controller
@@ -10,11 +11,20 @@ class RelatoriosController extends Controller
 
     public function index()
     {
-        //
+
     }
 
     public function gerarPdf()
     {
-        //
+        $teste = 'teste';
+        $teste2 = 'teste2';
+
+        $dompdf = new Dompdf();
+
+        $html = view('admin.pages.relatorios.modelo-pdf', compact('teste', 'teste2'));
+
+        $dompdf->loadHtml($html);
+        $dompdf->render();
+        $dompdf->stream();
     }
 }

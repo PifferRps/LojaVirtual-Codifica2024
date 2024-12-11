970dc23f9b2e61f4fdf35fcfa33fc416c9e8fffa
João Pedro V. Pansiere <ojp027@gmail.com>
2024-12-11 14:55:27 -0300

    Caso Admin, botões da home muda para 'configurações

--- resources/views/site/_components/navbar.blade.php
+++ resources/views/site/_components/navbar.blade.php
@@ -27,12 +27,17 @@
                 </div>
 
                 <div class="user-info__actions">
-                <a href="{{ route('site.meu-perfil.index') }}">
-                    <i class="fas fa-user-circle"></i> Meu Perfil</a>
-                <a href="{{route('pedidos.index')}}">
-                        <i class="fas fa-shopping-bag"></i> Minhas Compras</a>
-                <a href="{{ route('logout') }}">
-                        <i class="fas fa-sign-out-alt"></i> Sair</a>
+                    @if(Auth::user()->user_level == 999)
+                    <a href="{{ route('dashboard.index') }}">
+                        <i class="fas fa-user-circle"></i> Configurações</a>
+                    @else
+                    <a href="{{ route('site.meu-perfil.index') }}">
+                        <i class="fas fa-user-circle"></i> Meu Perfil</a>
+                    @endif
+                    <a href="{{route('pedidos.index')}}">
+                            <i class="fas fa-shopping-bag"></i> Minhas Compras</a>
+                    <a href="{{ route('logout') }}">
+                            <i class="fas fa-sign-out-alt"></i> Sair</a>
                 </div>
             </div>
         @else

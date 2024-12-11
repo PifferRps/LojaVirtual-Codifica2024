b42cb257e9e1a5fe36377c6e68671d2c820047d9
Jpbelloti <jpbelloti12@gmail.com>
2024-12-11 20:13:20 -0300

    Estilização Editar Senha - 160

--- resources/views/site/pages/perfil/editar-senha.blade.php
+++ resources/views/site/pages/perfil/editar-senha.blade.php
@@ -1,11 +1,11 @@
 @extends("site._layouts.perfil")
 @section("conteudoPerfil")
-    <form action="" style="display: flex; flex-direction: column" >
-        <label for="senhaAntiga">Senha antiga</label>
+    <form action="" style="display: flex; flex-direction: column" class="header_container__search dadosClientesPrincipal">
+        <label for="senhaAntiga">Senha Antiga</label>
         <input type="password"><br>
-        <label for="senhaNova">Nova senha</label>
+        <label for="senhaNova">Nova Senha</label>
         <input type="password"><br>
-        <label for="senhaNova">Confirmar nova senha</label>
+        <label for="senhaNova">Confirmar Nova Senha</label>
         <input type="password"><br>
         <button>Salvar</button>
     </form>

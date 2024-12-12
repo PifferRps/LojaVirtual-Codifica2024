605fba45de7fea2e9b333fe7daf7fbadea071dcb
João Pedro V. Pansiere <ojp027@gmail.com>
2024-12-12 09:01:28 -0300

    Estilizando a side bar/layout admin

--- resources/views/admin/_components/sidebar.blade.php
+++ resources/views/admin/_components/sidebar.blade.php
@@ -1,18 +1,12 @@
 <div class="sidebar">
-    <div class="sidebar_logo">
-        <img src="{{ asset('img/codificamaislogo2.png') }}" alt="codifica">
-    </div>
-
+    <img class="sidebar_img" src="{{ asset('img/codificamaislogo2.png') }}" alt="nome da loja na logo">
     <div class="sidebar_opcoes">
-        <a href="{{ route('dashboard.index') }}">Dashboard</a>
+        <a href="{{ route('dashboard.index') }}" style="border-top: 0.120rem solid rgb(167, 48, 48);">Dashboard</a>
         <a href="{{ route('produtos.index') }}">Produtos</a>
         <a href="{{ route('pedidos.index') }}">Pedidos</a>
         <a href="{{ route('clientes.index') }}">Clientes</a>
         <a href="{{ route('categorias.index') }}">Categorias</a>
         <a href="{{ route('relatorios.index') }}">Relátorios</a>
-    </div>
-
-    <div class="sidebar_baixo">
-        <a href="/" target="_blank">Ver loja</a>
+        <a href="/" target="_blank" style="font-weight: bold">Ver loja</a>
     </div>
 </div>

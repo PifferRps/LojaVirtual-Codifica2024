4833de724a32621658ad800efde4b983398cb06a
Jpbelloti <jpbelloti12@gmail.com>
2024-12-12 20:36:09 -0300

    Editar Senha - 160

--- resources/views/site/pages/perfil/editar-senha.blade.php
+++ resources/views/site/pages/perfil/editar-senha.blade.php
@@ -1,13 +1,13 @@
 @extends("site._layouts.perfil")
 @section("conteudoPerfil")
-    <form action="" style="display: flex; flex-direction: column" class="header_container__search dadosClientesPrincipal">
+    <form action="" class="informacoes dadosClientesPrincipal">
         <label for="senhaAntiga">Senha Antiga</label>
         <input type="password"><br>
         <label for="senhaNova">Nova Senha</label>
         <input type="password"><br>
         <label for="senhaNova">Confirmar Nova Senha</label>
         <input type="password"><br>
-        <button>Salvar</button>
+        <button class=salvar>Salvar</button>
     </form>
 @endsection
 
